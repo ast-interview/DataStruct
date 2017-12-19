bf617e3291839ed30995903025c67c7d6e9176a8
ivanli1990 <lilinfeng@lilinfengdeMacBook-Air.local>
2017-12-19 23:43:48 +0800

    add OrderStack

---
+++ stack/main/OrderStack.py
@@ -0,0 +1,38 @@
+# coding: utf-8
+'''
+@Author: Ivanli
+@Time:   2017.12.19
+'''
+class OrderStack(object):
+
+    # 初始化，建立空栈
+    def __init__(self):
+        pass
+
+    # 销毁栈
+    def destroyStack(self):
+        pass
+
+    # 清空栈
+    def clearStack(self):
+        pass
+
+    # 栈是否为空
+    def stackIsEmpty(self):
+        pass
+
+    # 获取栈顶元素
+    def getTop(self):
+        pass
+
+    # 入栈
+    def push(self, data):
+        pass
+
+    # 出栈
+    def pop(self, data):
+        pass
+
+    # 统计栈元素的个数
+    def getLength(self):
+        pass
