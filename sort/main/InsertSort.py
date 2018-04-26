3486155dd431f0cbb003f8cc772466dfe906ab36
ivanli1990 <lilinfeng@lilinfengdeAir.lan>
2018-04-27 01:00:55 +0800

    Add insertSort

--- sort/main/InsertSort.py
+++ sort/main/InsertSort.py
@@ -2,6 +2,11 @@
 
 '''
 插入排序法
+思想：
+1、从第N个元素开始，第n个元素之前的元素认为是已经排好顺序的了
+2、第N个元素与前n个有序元素比较，从后往前比较
+3、若第N个元素<前面的元素，则该前面元素后移1位（腾空地）
+4、循环往复步骤2，直到第N个元素>前面的元素，第N个元素插入该前面元素的后面1个位置
 '''
 
 class InsertSort(object):
@@ -15,18 +20,41 @@ class InsertSort(object):
         升序
         :return:
         '''
-        for i in range(self.len):
-            tmp_max = self.sq_list[i]
-            for j in range(i+1, self.len):
-                if tmp_max > self.sq_list[j]:
-                    self.sq_list[j-1] = self.sq_list[j]
+        j = 0
+        for i in range(1, self.len):  # 从第2个元素开始比较，因为第1个元素被认为是已排好序的长度为1的序列
+            cmp_elem = self.sq_list[i]  # 待插入的元素
+            for j in range(i-1, -1, -1):  # 从已排好序的前面元素序列(长度为i-1)依次往前比较
+                if cmp_elem < self.sq_list[j]:  # 若小于前面元素，则前面元素往后移1位（腾空地）
+                    self.sq_list[j+1] = self.sq_list[j]
                 else:
-                    self.sq_list[j-1] = tmp_max
+                    break  # 若大于前面元素，则终止循环，不再比较，将元素插入前面元素的后1位
+            if j == 0:  # 若比所有前面元素都小，则插入到首位
+                self.sq_list[0] = cmp_elem
+            else:
+                self.sq_list[j+1] = cmp_elem  # 插入前面元素的后1位
+
+    def desc(self):
+        '''
+        降序
+        :return:
+        '''
+        j = 0
+        for i in range(1, self.len):
+            cmp_elem = self.sq_list[i]
+            for j in range(i-1, -1, -1):
+                if cmp_elem > self.sq_list[j]:
+                    self.sq_list[j+1] = self.sq_list[j]
+                else:
+                    break
+            if j == 0:
+                self.sq_list[0] = cmp_elem
+            else:
+                self.sq_list[j+1] = cmp_elem
 
 if __name__ == "__main__":
-    sq_list = [9, 1, 5, 8, 3, 7, 4, 6, 2]
+    sq_list = [10, 7, 5, 3, 2, 1, 4, 6, 8, 9]
     insert = InsertSort(sq_list)
     insert.asc()
     print insert.sq_list
-    # insert.desc()
-    # print insert.sq_list
+    insert.desc()
+    print insert.sq_list
---
+++ sort/main/Sort.py
@@ -0,0 +1,92 @@
+# coding: utf-8
+
+class Sort(object):
+
+    def __init__(self, list):
+        self.len = len(list)
+        self.list = list
+
+    def bubbleAsc(self):
+
+        for i in range(self.len):
+            for j in range(self.len-1-i):
+                if self.list[j] > self.list[j+1]:
+                    tmp = self.list[j]
+                    self.list[j] = self.list[j+1]
+                    self.list[j+1] = tmp
+
+    def bubbleDesc(self):
+        for i in range(self.len):
+            for j in range(self.len-1-i):
+                if self.list[j] < self.list[j+1]:
+                    tmp = self.list[j]
+                    self.list[j] = self.list[j+1]
+                    self.list[j+1] = tmp
+
+    def selectAsc(self):
+        for i in range(self.len):
+            index_max = 0  # 最大元素的下标
+            max = self.list[index_max]  # 最大元素
+            for j in range(0, self.len-i):
+                if max < self.list[j]:
+                    max = self.list[j]  # 更新最大元素
+                    index_max = j  # 更新最大元素下标
+            # 将最大元素与无序序列的最后一个元素交换
+            tmp = self.list[index_max]
+            self.list[index_max] = self.list[j]
+            self.list[j] = tmp
+
+    def selectDesc(self):
+        for i in range(self.len):
+            index_min = 0
+            min = self.list[index_min]
+            for j in range(0, self.len-i):
+                if min > self.list[j]:
+                    index_min = j
+                    min = self.list[j]
+
+            tmp = self.list[index_min]
+            self.list[index_min] = self.list[j]
+            self.list[j] = tmp
+
+    def insertAsc(self):
+        for i in range(1, self.len):
+            cmp_elem = self.list[i]
+            for j in range(i-1, -1, -1):
+                if cmp_elem < self.list[j]:
+                    self.list[j+1] = self.list[j]
+                else:
+                    break
+            if j == 0:
+                self.list[0] = cmp_elem
+            else:
+                self.list[j+1] = cmp_elem
+
+    def insertDesc(self):
+        for i in range(1, self.len):
+            cmp_elem = self.list[i]
+            for j in range(i-1, -1, -1):
+                if cmp_elem > self.list[j]:
+                    self.list[j+1] = self.list[j]
+            if j == 0:
+                self.list[0] = cmp_elem
+            else:
+                self.list[j+1] = cmp_elem
+
+
+
+if __name__ == "__main__":
+    sq_list = [10, 7, 5, 3, 2, 1, 4, 6, 8, 9]
+    sort = Sort(sq_list)
+    sort.bubbleAsc()
+    print sort.list
+    sort.bubbleDesc()
+    print sort.list
+    sort.selectAsc()
+    print sort.list
+    sort.selectDesc()
+    print sort.list
+    sort.insertAsc()
+    print sort.list
+    sort.insertDesc()
+    print sort.list
